9a04250fcdc9e8a5b2728fb49e7c9daf5800e4db
guimmcastro <guimmcastro@gmail.com>
2022-08-24 18:48:59 -0300

    adicao console.log()

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -4,3 +4,9 @@ let email = "guimmcastro@gmail.com"
 let profissao = "engenheiro"
 let idade = 33
 let lugar = "Italia"
+
+console.log (nome)
+console.log (email)
+console.log (profissao)
+console.log (idade)
+console.log (lugar)
