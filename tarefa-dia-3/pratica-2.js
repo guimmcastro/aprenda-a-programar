c2528304da0806f3529e530604ea906c9314fc2d
guimmcastro <guimmcastro@gmail.com>
2022-08-28 17:59:14 -0300

    update pratica-2.js

--- tarefa-dia-3/pratica-2.js
+++ tarefa-dia-3/pratica-2.js
@@ -4,9 +4,9 @@ let multiplicador = 9
 let n = 10 // multiplicar de 1 a n
 let i = 1
 
-while(i<=n){
+while (i <= n) {
     let produto = multiplicador * i
-    let exibicao = '${multiplicador} x ${i} = ${produto}'
+    let exibicao = `${multiplicador} x ${i} = ${produto}`
     console.log(exibicao)
     i++
 }
